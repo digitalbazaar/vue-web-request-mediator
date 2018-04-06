6f61265025481108c050c050f6812f64710eace4
Dave Longley <dlongley@digitalbazaar.com>
2018-04-05 22:59:16 -0400

    Add core files.

---
+++ WrmCloseButton.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <svg>
+      <circle cx="9.5" cy="9.5" r="8.5" />
+      <path d="M5, 5, 14, 14" />
+      <path d="M5, 14, 14, 5" />
+    </svg>
+  </div>
+</template>
+<script>
+/*!
+ * New BSD License (3-clause)
+ * Copyright (c) 2018, Digital Bazaar, Inc.
+ * All rights reserved.
+ */
+'use strict';
+
+export default {
+  name: 'WrmCloseButton'
+};
+</script>
+<style>
+</style>
