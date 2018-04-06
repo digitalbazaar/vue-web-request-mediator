56e46a4ab36d8a23b5baeb9f76438ff179c4a001
Dave Longley <dlongley@digitalbazaar.com>
2018-04-05 23:02:33 -0400

    Add activity bar component.

---
+++ WrmActivityBar.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="wrm-activity-bar">
+    <div class="wrm-activity-line"></div>
+  </div>
+</template>
+<script>
+/*!
+ * New BSD License (3-clause)
+ * Copyright (c) 2018, Digital Bazaar, Inc.
+ * All rights reserved.
+ */
+'use strict';
+
+export default {
+  name: 'WrmActivityBar'
+};
+</script>
+<style>
+</style>
